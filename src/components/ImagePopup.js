26ed68c6eef9649e66c4977102d00319a87f71ad
azatprojazz <azatprojazz@gmail.com>
2023-03-02 15:48:16 +0300

    feat: добавить компонент ImagePopup

---
+++ src/components/ImagePopup.js
@@ -0,0 +1,18 @@
+function ImagePopup({ card, onClose }) {
+  return (
+    <div className={`popup popup_type_view-card ${card.name ? 'popup_opened' : ''}`}>
+      <div className="popup__view-container">
+        <button
+          className="popup__close-btn opacity-on-hover"
+          type="button"
+          aria-label="Закрыть"
+          onClick={onClose}
+        ></button>
+        <img className="popup__image" src={card.link} alt={card.name} />
+        <p className="popup__caption">{card.name}</p>
+      </div>
+    </div>
+  );
+}
+
+export default ImagePopup;
