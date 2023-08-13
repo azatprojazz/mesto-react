35b7eb2686c59014696ad617493cc4f6b233ad19
azatprojazz <azatprojazz@gmail.com>
2023-08-13 18:27:59 +0300

    fix: объединение методов запросов в _request и добавление комментариев для лучшей читаемости кода

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,72 +4,68 @@ class Api {
     this._headers = options.headers;
   }
 
+  // Обрабатывает ответ от сервера
+  // Если ответ успешный (с кодом статуса 200-299), преобразует его в JSON
+  // В противном случае возвращает отклоненный промис с сообщением об ошибке
   _response(res) {
     return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`);
   }
 
+  // Выполняет запрос к серверу
+  // Принимает три аргумента:
+  // 1. endpoint: строка, содержащая конечную точку (путь) API
+  // 2. method: строка, содержащая HTTP-метод (например, 'GET', 'POST', 'PATCH', 'DELETE'), по умолчанию 'GET'
+  // 3. body: объект с данными, которые будут отправлены в теле запроса (если это применимо, например, для 'POST' и 'PATCH' запросов)
+  _request(endpoint, method = 'GET', body = null) {
+    // Создает объект с параметрами запроса, включая метод и заголовки
+    const options = {
+      method,
+      headers: this._headers,
+    };
+    // Если передано тело запроса, преобразует его в строку JSON и добавляет к параметрам запроса
+    if (body) {
+      options.body = JSON.stringify(body);
+    }
+    // Выполняет запрос с использованием fetch, передавая базовый URL, конечную точку и параметры запроса
+    // Затем обрабатывает ответ, используя метод _response
+    return fetch(`${this._baseUrl}${endpoint}`, options).then((res) => {
+      return this._response(res);
+    });
+  }
+
+  // Получает информацию о пользователе
   getUserInfo() {
-    return fetch(`${this._baseUrl}/users/me`, {
-      headers: this._headers,
-    }).then((res) => {
-      return this._response(res);
-    });
+    return this._request('/users/me');
   }
 
+  // Редактирует информацию о пользователе
   editUserInfo({ name, about }) {
-    return fetch(`${this._baseUrl}/users/me`, {
-      method: 'PATCH',
-      headers: this._headers,
-      body: JSON.stringify({ name, about }),
-    }).then((res) => {
-      return this._response(res);
-    });
+    return this._request('/users/me', 'PATCH', { name, about });
   }
 
+  // Получает начальные карточки
   getInitialCards() {
-    return fetch(`${this._baseUrl}/cards`, {
-      headers: this._headers,
-    }).then((res) => {
-      return this._response(res);
-    });
+    return this._request('/cards');
   }
 
+  // Добавляет новую карточку
   addNewCard({ name, link }) {
-    return fetch(`${this._baseUrl}/cards`, {
-      method: 'POST',
-      headers: this._headers,
-      body: JSON.stringify({ name, link }),
-    }).then((res) => {
-      return this._response(res);
-    });
+    return this._request('/cards', 'POST', { name, link });
   }
 
+  // Устанавливает новый аватар
   setNewAvatar({ avatar }) {
-    return fetch(`${this._baseUrl}/users/me/avatar`, {
-      method: 'PATCH',
-      headers: this._headers,
-      body: JSON.stringify({ avatar }),
-    }).then((res) => {
-      return this._response(res);
-    });
+    return this._request('/users/me/avatar', 'PATCH', { avatar });
   }
 
+  // Удаляет карточку
   deleteCard(cardId) {
-    return fetch(`${this._baseUrl}/cards/${cardId}`, {
-      method: 'DELETE',
-      headers: this._headers,
-    }).then((res) => {
-      return this._response(res);
-    });
+    return this._request(`/cards/${cardId}`, 'DELETE');
   }
 
+  // Добавляет/удаляет лайк карточке
   addLikeCard(cardId, isLiked) {
-    return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
-      method: `${isLiked ? 'PUT' : 'DELETE'}`,
-      headers: this._headers,
-    }).then((res) => {
-      return this._response(res);
-    });
+    return this._request(`/cards/${cardId}/likes`, isLiked ? 'PUT' : 'DELETE');
   }
 }
 
